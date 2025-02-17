f6c72afe0a300f9a4deafae1e47ebc4c76e02664
Kumar Bijayananda <kumarbijayananda@gmail.com>
2025-02-17 14:56:25 -0500

    fav btn and go to fav on search result

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,44 +1,68 @@
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import importPoke from "../utility/ImportPoke";
 
 export default function Search() {
   const { query } = useParams();
   const [searchRes, setSearchRes] = useState([]);
+  const [favs, setFavs] = useState([]);
 
-  useEffect(()=>{
+  useEffect(() => {
     try {
-        async function searchForPoke(){
-            const res= await importPoke(query);
-            setSearchRes(res)
-        }
+      async function searchForPoke() {
+        const res = await importPoke(query);
+        setSearchRes(res);
+      }
 
-        searchForPoke();
-
+      searchForPoke();
     } catch (error) {
-        console.error(error)        
+      console.error(error);
     }
-    },[query])
+  }, [query]);
 
-   
+  const handleFav = (card) => {
+    // console.log(card)
+    const currentCard = card;
+    const isFav = favs.some((fav) => fav.id === currentCard.id);
+
+    let updatedFavs;
+    if (isFav) {
+      updatedFavs = favs.filter((fav) => fav.id !== currentCard.id);
+    } else {
+      updatedFavs = [...favs, currentCard];
+    }
+
+    setFavs(updatedFavs);
+    localStorage.setItem("favs", JSON.stringify(updatedFavs));
+  };
 
   return (
     <>
-       <div className="searchContainer">
-          <div className="title"><h2>Search</h2></div>
-          <div className="searchDiv">
-            {searchRes.length > 0 ? (
-              searchRes.map((card) => (
-                <div key={card.id} className="searchCard">
-                  <img src={card.images.small} alt={card.name} />
-                  <p>{card.name}</p>
-                </div>
-              ))
-            ) : (
-              <p>No search result for `${query}`.</p>
-            )}
-          </div>
+      <div className="searchContainer">
+        <div className="title">
+          <h2>Displaying {searchRes.length} cards that match search</h2>
         </div>
+        <div className="searchDiv">
+          {searchRes.length > 0 ? (
+            searchRes.map((card) => (
+              <div key={card.id} className="searchCard">
+                <button className="cardBtn" onClick={() => handleFav(card)}>
+                  {favs.some((fav) => fav.id === card.id)
+                    ? "Remove from Favs"
+                    : "Add to Favs"}
+                </button>
+
+                <img src={card.images.small} alt={card.name} />
+                <Link to="/favorites">
+                  <button className="cardBtn">Go to Favs</button>
+                </Link>
+              </div>
+            ))
+          ) : (
+            <p>No search result for `${query}`.</p>
+          )}
+        </div>
+      </div>
     </>
   );
 }
